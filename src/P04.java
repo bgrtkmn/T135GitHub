f99e84151caded153d10a0c09c7fbfcf459e6fa5
babayigit <babayigit@gmail.com>
2024-02-29 12:30:15 +0300

    muhammed

--- src/P04.java
+++ src/P04.java
@@ -2,5 +2,6 @@ public class P04 {
     public static void main(String[] args) {
 
         System.out.println("Ne yaparsan Yap AŞK ile YAP!");
+        System.out.println("Versiyon olsun diye...");
     }
 }
